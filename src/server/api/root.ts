eeabb8808b2f3ccf011d1258c74297d1ce73aa67
Balduin <bb@nessy.online>
2024-10-20 19:54:04 +0200

    data layer

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { projectRouter } from "./routers/projects";
+import { postRouter } from "./routers/post";
 
 /**
  * This is the primary router for your server.
@@ -7,6 +8,7 @@ import { projectRouter } from "./routers/projects";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+    post: postRouter,
     projects: projectRouter,
 });
 
--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -1,4 +1,4 @@
-import { CreateProjectSchema } from "@/features/project/schema";
+import { CreateProjectSchema, ProjectSchema } from "@/features/project/schema";
 import { createTRPCRouter, withOrgProcedure } from "@/server/api/trpc";
 import { projects } from "@/server/db/schema";
 import { createID } from "@/server/ids";
@@ -10,13 +10,27 @@ export const projectRouter = createTRPCRouter({
             orderBy: (projects, { desc }) => [desc(projects.createdAt)],
         });
     }),
+    get: withOrgProcedure
+        .input(ProjectSchema.shape.id)
+        .query(async ({ ctx, input }) => {
+            return ctx.db.query.projects.findFirst({
+                where: (proj, { eq, and }) =>
+                    and(eq(proj.orgId, ctx.auth.orgId), eq(proj.id, input)),
+                with: {},
+            });
+        }),
     create: withOrgProcedure
         .input(CreateProjectSchema)
         .mutation(async ({ ctx, input }) => {
-            return ctx.db.insert(projects).values({
-                id: createID("project"),
-                name: input.name,
-                orgId: ctx.auth.orgId,
-            });
+            const inserted = await ctx.db
+                .insert(projects)
+                .values({
+                    id: createID("project"),
+                    name: input.name,
+                    orgId: ctx.auth.orgId,
+                    ownerId: ctx.auth.userId,
+                })
+                .returning();
+            return inserted[0]!;
         }),
 });
